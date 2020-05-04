bc43175da0cae37aa272c117ce749f279dd94518
SuperMario <44363781+Supermariozmx@users.noreply.github.com>
2020-05-05 00:12:14 +0800

    [0503]add home login  admin

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from "react";
 import {BrowserRouter,Route,Switch} from "react-router-dom";
-import Login from "./pages/login/login";
+import WrapLogin from "./pages/login/login";
 import Homepage from "./pages/home/homepage";
 import Admin from "./pages/admin/admin";
 
@@ -14,7 +14,7 @@ export default class App extends Component {
             <BrowserRouter>
             <Switch>
             <Route path="/" exact component={Homepage}></Route>
-            <Route path="/login" component={Login}></Route>
+            <Route path="/login" component={WrapLogin}></Route>
             <Route path="/admin" component={Admin}></Route>
             </Switch>
             </BrowserRouter>
